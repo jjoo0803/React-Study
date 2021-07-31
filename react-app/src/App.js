65087d3f0ae94003a0978c6838b52b526574340f
jjoo0803 <jhjh083@naver.com>
2021-07-31 22:53:25 +0900

    test commit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,44 @@
+import React, { Component } from 'react'; 
 import logo from './logo.svg';
 import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p> */}
+        </header>
+        <div>
+          test
+        </div>
+      </div>
+    )
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor (props) {
+    super(props)
+    this.state = {
+      personData: [
+        {
+          name : 'Alex',
+          age: 20,
+          phone : '010-0000-0000',
+        },
+        {
+          name : 'Peter',
+          age: 14,
+          phone : '010-0000-0001',
+        },
+        {
+          name : 'Hann',
+          age: 50,
+          phone : '010-0000-0002',
+        }
+      ]
+    }
+  }
 }
-
 export default App;
