dc21c87a2fc319dba2456c429051b8f785e44f73
jjoo0803 <jhjh083@naver.com>
2021-08-07 21:59:19 +0900

    - TodoTemplate.js css 변경  - TodoInsert.js css 변경  - TodoTitle.js css 변경

--- react-app/src/todolist/TodoInsert.js
+++ react-app/src/todolist/TodoInsert.js
@@ -1,17 +1,48 @@
 import { Component } from 'react';
-import { MdAdd } from 'react-icons/md'
+// import { GrAdd } from 'react-icons/gr'
 import './TodoInsert.scss'
 
 class TodoInsert extends Component {
   render () {
     return (
-      <form className="todo-insert">
-        <input placeholder="할 일을 입력하세요" />
-        <button>
-          <MdAdd />
+      <form className="todo-insert" onSubmit={this.handleSubmit}>
+        <input
+          placeholder="add a new task"
+          // onChange={this.handleChange}
+        />
+        <button
+          type="submit"
+          onClick={this.handleBtnClick}
+        >
+          add
         </button>
       </form>
     )
+  }
+
+  constructor (props) {
+    super(props)
+    // 생성자에서 함수 바인딩 해줘야됨
+    // this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleBtnClick = () => {
+
+  }
+  handleChange (e) {
+    // console.log('val: ', e.target.value)
+    // e.preventDefault()
+    // this.setState({
+    //   value: e.target.value
+    // })
+  }
+
+  handleSubmit = (e) => {
+    // 생성자 함수에서 바인딩 하지 않는 경우 화살표 함수로 구현 (자동으로 바인딩 됨)
+    e.preventDefault() // submit 하는 경우 새로고침 방지
   }
 }
 
---
+++ react-app/src/todolist/TodoListItem.js
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+import './TodoListItem.scss'
+
+class TodoListItem extends Component {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default TodoListItem
