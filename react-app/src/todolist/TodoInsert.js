9c368b5b1f541d355b5e8f57e2cea06743c9026e
jjoo0803 <jhjh083@naver.com>
2021-08-14 23:03:31 +0900

    Todo List add 기능 추가  - prop를 통해 부모 -> 자식 함수 전달  - add 버튼 선택 시 부모 함수 호출하여 list에 추가

--- react-app/src/todolist/TodoInsert.js
+++ react-app/src/todolist/TodoInsert.js
@@ -1,26 +1,34 @@
 
-import React from 'react';
+import React , { useState } from 'react';
 // import { GrAdd } from 'react-icons/gr'
 import './TodoInsert.scss'
 
-function TodoInsert() {
-
-  const handleBtnClick = () => {
-  }
+function TodoInsert (props) {
+  const { addList } = props
+  const [content, setContent] = useState('')
 
   const handleSubmit = (e) => {
+    if (content !== '') {
+      addList(content)
+      setContent('')
+    }
     e.preventDefault() // submit 하는 경우 새로고침 방지
   }
 
+  const handleChange = (e) => {
+    setContent(e.target.value)
+  }
+
   return (
-    <form className="todo-insert" onSubmit={handleSubmit}>
+    <form className="todo-insert">
       <input
         placeholder="add a new task"
-        // onChange={this.handleChange}
+        onChange={handleChange}
+        value={content}
       />
       <button
         type="submit"
-        onClick={handleBtnClick}
+        onClick={handleSubmit}
       >
         add
       </button>
--- react-app/src/todolist/TodoTemplate.js
+++ react-app/src/todolist/TodoTemplate.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './TodoTemplate.scss';
 
 
@@ -8,23 +8,31 @@ import TodoList from './TodoList'
 
 
 function TodoTemplate() {
-  const [todos, setTodos] = useState([
-    {
-      idx: 1,
-      content: 'todo========1',
+  /**
+   * useRef
+   * 1. 특정 DOM 선택 및 조작
+   * 2. component 안에서 수정 및 조회 할 수 있는 로컬 변수
+   *    - useRef로 관리되는 변수는 설정 후 바로 조회 가능
+   *    - ref 안의 값이 변경되어도 컴포넌트는 렌더링이 일어나지 않는다
+   */
+  const todoIdx = useRef(1)
+  const [todos, setTodos] = useState([])
+
+  const addList = (text) => {
+    const todo = {
+      idx: todoIdx.current,
+      content: text,
       checked: false
-    },
-    {
-      idx: 2,
-      content: 'todo=======2',
-      checked: true
     }
-  ])
+    console.log(todo)
+    setTodos(todos.concat(todo))
+    todoIdx.current += 1
+  }
 
   return (
     <div className="todo-template">
       <TodoTitle />
-      <TodoInsert />
+      <TodoInsert addList={addList}/>
       <TodoList todos={todos}/>
     </div>
   )
